704c636b9b214dcafdac65cf84ff3ab5ef317d98
Luan <luan10017@hotmail.com>
2021-06-10 14:20:01 -0300

    Do exercisis

--- exercicio-12/app.js
+++ exercicio-12/app.js
@@ -27,6 +27,17 @@ const best2019Movies = [
   { title: 'A Vida Invisível', directedBy: 'Karim Aïnouz' }
 ]
 
+let message = 'Segundo o site Omelete, os melhores filmes de 2019 são:'
+
+const generateMovieMessage = movi => {
+  message += `
+  - ${movi.title}, dirigido por ${movi.directedBy}`
+}
+
+best2019Movies.forEach(generateMovieMessage)
+
+console.log(message)
+
 /*
   02
 
@@ -59,16 +70,22 @@ const youtubeUser = {
       gitHub: 'https://github.com/Roger-Melo'
     },
     country: 'Brasil'
+  },
+  logRecenctVideos () {
+    console.log(`Vídeos recentes de ${this.name}:`)
+    this.videos.recentVideos.forEach(video => { console.log(video.title) })
   }
 }
 
+
+// youtubeUser.logRecenctVideos()
 /*
   03
 
   - Exiba o valor do PI no console.
 */
 
-
+// console.log(Math.PI)
 
 /*
   04
@@ -78,6 +95,7 @@ const youtubeUser = {
 */
 
 const firstNumber = 8.3
+// console.log(Math.ceil(firstNumber))
 
 /*
   05
@@ -87,6 +105,7 @@ const firstNumber = 8.3
 */
 
 const secondNumber = 3.5
+// console.log(Math.round(secondNumber))
 
 /*
   06
@@ -96,6 +115,7 @@ const secondNumber = 3.5
 */
 
 const thirdNumber = 8.9
+// console.log(Math.floor(thirdNumber))
 
 /*
   07
@@ -104,6 +124,7 @@ const thirdNumber = 8.9
 */
 
 const fourthNumber = 5.5
+// console.log(Math.trunc(fourthNumber))
 
 /*
   08
@@ -111,3 +132,5 @@ const fourthNumber = 5.5
   - A cada vez que o index.html for carregado, exiba no console um número  
     aleatório de 0 à 10, incluindo 0 e 10.
 */
+
+console.log(Math.round(Math.random()*10))
